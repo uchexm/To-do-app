1b44ddf4484ca6f86330a585474a13f993a1658c
uchexm <109084069+uchexm@users.noreply.github.com>
2022-12-07 08:18:44 +0200

    Add css style file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
 import _ from "lodash";
 import "./style.css";
-import Ic from "./ic.jpeg";
 
-function component() {
-  const element = document.createElement("div");
+const body = document.getElementById("todos-list");
+let taskno = 0;
+let taskLists = [];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Ic;
-
-  element.appendChild(myIcon);
-  return element;
+function printfn() {
+  body.replaceChildren();
+  const localtasks = JSON.parse(localStorage.getItem("tasks"));
+  taskLists = localtasks;
+  const index = 1;
+  localtasks.forEach((task) => {
+    const newDiv = document.createElement("li");
+    newDiv.innerHTML = `
+    <div class="tod" id="${task.id}">
+          <i class="fa fa-circle-thin" aria-hidden="true"></i>
+          <p id="checked" class="checked">
+            ${task.description}
+            <i id="af">
+              <i class="fa fa-pencil-square" aria-hidden="true"></i>
+              <i class="fa fa-trash" aria-hidden="true"></i>
+            </i>
+          </p>
+        </div>
+        <hr />
+    `;
+    body.append(newDiv);
+    taskno += 1;
+  });
 }
+if (localStorage.getItem("tasks")) {
+  printfn();
+}
+const addicn = document.querySelector(".fa-plus");
+const description = document.querySelector(".nt");
 
-document.body.appendChild(component());
+addicn.addEventListener("click", () => {
+  if (description.value !== "") {
+    taskLists.push({
+      index: `number${taskno}`,
+      description: description.value,
+      completed: false,
+    });
+    localStorage.setItem("tasks", JSON.stringify(taskLists));
+    printfn();
+    document.forms[0].reset();
+  }
+});
